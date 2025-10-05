ffd4304c1dc3895f1dcd2983682a165d71f844d1
Daria Zaimak <uennyfortest@gmail.com>
2025-10-04 17:49:46 -0700

    added Deck class

--- card_game/src/Deck.js
+++ card_game/src/Deck.js
@@ -7,16 +7,27 @@ export default class Deck {
   constructor() {
     // TODO: создайте this.cards как массив из 52 уникальных Card
     // Подсказка: два вложенных цикла по Card.ranks() и Card.suits()
+    this.cards = [];
+    for (const suit of Card.suits()) {
+        for (const rank of Card.ranks()) {
+            this.cards.push(new Card(rank, suit));
+        }
+    }
   }
 
   /** Текущее количество карт в колоде */
   get size() {
     // TODO
+    return this.cards.length;
   }
 
   /** Перемешать колоду (Fisher–Yates). Модифицирует this.cards, возвращает this. */
   shuffle() {
     // TODO: реализуйте алгоритм перебора с конца и обмена элементов
+    for (let i = this.cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
     return this;
   }
 
@@ -27,15 +38,26 @@ export default class Deck {
    */
   draw(count = 1) {
     // TODO: извлеките с конца массива. Проверьте, что карт хватает (RangeError).
+    if (this.size < count) {
+        throw new RangeError ("There are not enough cards!");
+    }
+    if (count === 1) {
+        return this.cards.pop();
+    } else {
+        return this.cards.splice(-count);
+    }
   }
 
   /** Сбросить список карт в низ колоды (например, после раунда) */
-  putToBottom(cards) {
+  putToBottom(hand) {
     // TODO: добавьте массив cards в начало или конец — на ваш выбор, но стабильно.
+    this.cards.unshift(...hand);
+    return this;
   }
 
   /** Вернуть плоский массив строк представлений (для отладки) */
   debugList() {
     // TODO: верните this.cards.map(c => c.toString())
+    return this.cards.map(c => c.toString());
   }
 }
